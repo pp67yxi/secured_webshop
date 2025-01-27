9db4ab05b25717e3f1a7640839a2e710aa86fc72
Alessandro <pp67yxi@eduvaud.ch>
2025-01-27 14:43:09 +0100

    feat: connection à la base de données MySQL

--- app/server.js
+++ app/server.js
@@ -16,3 +16,19 @@ app.use("/user", userRoute);
 https.createServer(sslOptions, app).listen(port, () => {
   console.log("Server running on port " + port);
 });
+
+// Connexion à MySQL
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  port: "6033",
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "secured_webshop",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to MySQL Database");
+});
